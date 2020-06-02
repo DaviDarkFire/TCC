f3d74154dce222a59a9304acc13a2917df7ce97f
Davi <daviferreirasantog@gmail.com>
2020-06-02 09:37:36 -0300

    removendo alguns arquivos desnecessários

--- teste.py
+++
@@ -1,7 +0,0 @@
-import os
-
-path = "C:\\Users\\davec\\Desktop\\banco_de_faces"
-for pinto in os.listdir(path):
-    if(os.path.isdir(os.path.join(path,pinto))):
-        for file in os.listdir(os.path.join(path,pinto)):
-            
